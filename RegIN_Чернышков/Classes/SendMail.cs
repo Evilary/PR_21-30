fe1f6ecaaccab730bd7ed2f2dc95629aea5bf632
df275 <df275@EVIL>
2026-05-10 19:15:33 +0500

    Разработанный класс отправки сообщения на почту. Чернышков.

---
+++ RegIN_Чернышков/Classes/SendMail.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace RegIN_Чернышков.Classes
+{
+    public class SendMail
+    {
+        public static void SendMessage(string message, string to)
+        {
+            var smtpClient = new SmtpClient("smtp.yandex.ru")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("yandex@yandex.ru", "password"),
+                EnableSsl = true
+            };
+
+            smtpClient.Send("kotboris140107@yandex.ru", to, "Проект RegIn", message);
+        }
+    }
+}
